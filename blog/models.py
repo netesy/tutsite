15e8d9ea30882c90e134f0c39da40ca2a8074e9e
Netesy [Maximus] Emmanuel <netesy1@gmail.com>
2016-03-05 18:12:10 +0100

    Updated Templates
    updated the templates

--- blog/models.py
+++ blog/models.py
@@ -12,5 +12,6 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-    def _str_(self):
+
+    def __str__(self):
         return self.title
